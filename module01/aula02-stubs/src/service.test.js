da5f5198ee262f73d15ac63725e5bd6c3d6b4588
Caio Tracera <ctrcra@gmail.com>
2021-06-10 17:13:48 -0300

    feat: add stubs to makeRequest method

---
+++ module01/aula02-stubs/src/service.test.js
@@ -0,0 +1,35 @@
+const sinon = require("sinon");
+
+const Service = require("./service");
+
+const BASE_URL_1 = "https://swapi.dev/api/planets/1/";
+const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
+
+const mocks = {
+  tatooine: require("./mocks/tatooine.json"),
+  alderaan: require("./mocks/alderaan.json"),
+};
+
+(async () => {
+  // {
+  //   /**
+  //    * Dessa forma, a função depende de conexão com a internet para buscar os dados
+  //    * na API do StarWars.
+  //    *
+  //    * Ou seja: não é recomendado!
+  //    */
+  //   const service = new Service();
+  //   const withoutStub = await service.makeRequest(BASE_URL_2);
+  //   console.log(JSON.stringify(withoutStub));
+  // }
+
+  const service = new Service();
+  const stub = sinon.stub(service, service.makeRequest.name);
+  stub.withArgs(BASE_URL_1).resolves(mocks.tatooine);
+  stub.withArgs(BASE_URL_2).resolves(mocks.alderaan);
+
+  {
+    const response = await service.makeRequest(BASE_URL_1);
+    console.log("response", response);
+  }
+})();
