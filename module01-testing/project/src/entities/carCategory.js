505f81d1f831ce8f7f5fa51c3d3501af853afc16
Caio Tracera <ctrcra@gmail.com>
2021-06-16 16:32:01 -0300

    feat(entities): add constructor to carCategory

--- module01-testing/project/src/entities/carCategory.js
+++ module01-testing/project/src/entities/carCategory.js
@@ -1,7 +1,12 @@
 const Base = require("./base/base");
 
 class CarCategory extends Base {
-  constructor({}) {}
+  constructor({ id, name, carIds, price }) {
+    super({ id, name });
+
+    this.carIds = carIds;
+    this.price = price;
+  }
 }
 
 module.exports = CarCategory;
