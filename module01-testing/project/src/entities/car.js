730e2ec790b7e4f12f84be48d7b0fe0680a6d5dd
Caio Tracera <ctrcra@gmail.com>
2021-06-16 16:31:05 -0300

    feat(entities): add constructor to car

--- module01-testing/project/src/entities/car.js
+++ module01-testing/project/src/entities/car.js
@@ -1,7 +1,13 @@
 const Base = require("./base/base");
 
 class Car extends Base {
-  constructor({}) {}
+  constructor({ id, name, releaseYear, available, gasAvailable }) {
+    super({ id, name });
+
+    this.releaseYear = releaseYear;
+    this.available = available;
+    this.gasAvailable = gasAvailable;
+  }
 }
 
 module.exports = Car;
