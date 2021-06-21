5ad5c31595729388a845bae221f08cf1126ab3dd
Caio Tracera <ctrcra@gmail.com>
2021-06-21 09:01:24 -0300

    feat: add calculateFinalPrice method

--- module01-testing/project/src/service/carService.js
+++ module01-testing/project/src/service/carService.js
@@ -1,9 +1,10 @@
 const BaseRepository = require("./../repository/base/baseRepository");
+const Tax = require("./../entities/tax");
 
 class CarService {
   constructor({ cars }) {
     this.carRepository = new BaseRepository({ file: cars });
-
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge;
     this.currencyFormat = new Intl.NumberFormat("pt-br", {
       style: "currency",
       currency: "BRL",
@@ -22,6 +23,17 @@ class CarService {
   async getAvailableCar(carCategory) {
     const carId = this.chooseRandomCar(carCategory);
     return this.carRepository.find(carId);
+  }
+
+  calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer;
+    const { price } = carCategory;
+    const { then: tax } = this.taxesBasedOnAge.find(
+      (tax) => age >= tax.from && age <= tax.to
+    );
+
+    const finalPrice = tax * price * numberOfDays;
+    return this.currencyFormat.format(finalPrice);
   }
 }
 
