ff9faa7537a745c34fafdf0b371141c59a21a6cb
Caio Tracera <ctrcra@gmail.com>
2021-06-28 21:26:27 -0300

    feat: add generateInstanceFromString method

--- module02-life_cycle/project/src/person.js
+++ module02-life_cycle/project/src/person.js
@@ -35,4 +35,17 @@ export class Person {
       }).format(mapDate(this.to)),
     };
   }
+
+  static generateInstanceFromString(text) {
+    const EMPTY_SPACE = "";
+    const [id, vehicles, kmTraveled, from, to] = text.split(EMPTY_SPACE);
+
+    return new Person({
+      id,
+      kmTraveled,
+      from,
+      to,
+      vehicles: vehicles.split(","),
+    });
+  }
 }
