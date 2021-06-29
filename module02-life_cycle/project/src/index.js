ccb2888fc434fde2d15ccc4ca007b36762291360
Caio Tracera <ctrcra@gmail.com>
2021-06-28 21:21:31 -0300

    feat: add main loop

---
+++ module02-life_cycle/project/src/index.js
@@ -0,0 +1,26 @@
+import { TerminalController } from "./controllers/TerminalController.js";
+import database from "./../database.json";
+
+const DEFAULT_LANG = "pt-BR";
+const STOP_TERM = ":q";
+
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
+
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question();
+    if (answer === STOP_TERM) {
+      terminalController.closeTerminal();
+      console.log("process finished!");
+      return;
+    }
+
+    return mainLoop();
+  } catch (error) {
+    console.log("DEU RUIM**", error);
+    return mainLoop();
+  }
+}
+
+await mainLoop();
